6f1fbef6a4d268eeb9d41aad8586455edffc2133
Fendry Tonrate <ftonrate91@gmail.com>
2025-08-28 23:09:11 +0700

    Update ConditionalNavbar.tsx
    fix

--- src/components/core/ConditionalNavbar.tsx
+++ src/components/core/ConditionalNavbar.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import { usePathname } from "next/navigation";
-import Navbar from "@/components/core/Navab";
+import Navbar from "@/components/core/Navabar";
 
 export default function ConditionalNavbar() {
   const pathname = usePathname() || "/";
