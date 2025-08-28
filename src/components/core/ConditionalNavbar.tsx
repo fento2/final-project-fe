36e49b60e734dd4cf58014170af6b2e135abe8c0
fento2 <ftonrate91@gmail.com>
2025-08-28 23:00:47 +0700

    test

--- src/components/core/ConditionalNavbar.tsx
+++ src/components/core/ConditionalNavbar.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import { usePathname } from "next/navigation";
-import Navbar from "@/components/core/Navbar";
+import Navbar from "@/components/core/Navab";
 
 export default function ConditionalNavbar() {
   const pathname = usePathname() || "/";
