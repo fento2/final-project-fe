a312cd26ba81d8a49a50f5da9ff9a22361c29902
fento2 <ftonrate91@gmail.com>
2025-08-28 23:30:11 +0700

    test

--- src/app/dashboard/profile/page.tsx
+++ src/app/dashboard/profile/page.tsx
@@ -2,7 +2,7 @@ const DashboardUser = () => {
   return (
     <>
       <div className="bg-amber-700">
-        <p className="text-white">testing tetifsdfsd</p>
+        <p className="text-white">testing tetifssd</p>
       </div>
     </>
   );
