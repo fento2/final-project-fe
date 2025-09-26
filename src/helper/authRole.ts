92954865ad2ef7a76f2ae49e75200dc5130e9ad3
fento2 <ftonrate91@gmail.com>
2025-09-26 20:24:08 +0700

    test

--- src/helper/authRole.ts
+++ src/helper/authRole.ts
@@ -5,18 +5,25 @@
 //     return true;
 //   }
 // };
-
+"use client";
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { useAuthStore } from "@/lib/zustand/authStore";
 
-export const useAuthRole = (validRole: string) => {
-  const { role, checkLogin, isLogin } = useAuthStore();
+export const useAuthRole = (validRole: string): void => {
+  const { role, checkLogin, isLogin } = useAuthStore((state) => ({
+    role: state.role,
+    checkLogin: state.checkLogin,
+    isLogin: state.isLogin,
+  }));
+
   const router = useRouter();
 
   useEffect(() => {
+    // Kalau sedang cek login, tunggu dulu
     if (checkLogin) return;
 
+    // Redirect kalau user tidak login atau role tidak sesuai
     if (!isLogin || role !== validRole) {
       router.replace("/");
     }
