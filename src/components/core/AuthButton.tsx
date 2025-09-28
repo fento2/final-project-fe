91d1a000721fc24b0c1e55cc73c91dc620cc6b95
fento2 <ftonrate91@gmail.com>
2025-09-28 21:44:07 +0700

    salah developr

--- src/components/core/AuthButton.tsx
+++ src/components/core/AuthButton.tsx
@@ -68,7 +68,7 @@ export default function AuthButtons() {
                                 {/* Menu Items dengan hover animasi */}
                                 <div className="flex flex-col gap-1">
                                     <motion.div whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }}>
-                                        {role === 'COMPANY' && <DeveloperOption />}
+                                        {role === 'DEVELOPER' && <DeveloperOption />}
                                         {role === 'USER' && <UserOption />}
                                         {role === 'COMPANY' && <CompanyOption />}
                                     </motion.div>
