73c036108422ee5e5bee37f8116563eb1eb57ff2
mkahfiyp <mkahfi467@gmail.com>
2025-10-01 23:15:27 +0700

    perbaiki sistem subscription di cv generator

--- src/hooks/useSubscription.ts
+++ src/hooks/useSubscription.ts
@@ -1,14 +1,19 @@
 import { useEffect, useState } from "react";
 import { apiCall } from "@/helper/apiCall";
+import { UserSubscriptionActiveDTO } from "@/types/userSubscription";
 export function useSubscription() {
     const [active, setActive] = useState<boolean | null>(null);
     useEffect(() => {
         let mounted = true;
         (async () => {
             try {
-                const res = await apiCall.get("/userSubscription");
-                const data = res.data?.data;
-                if (mounted) setActive(Array.isArray(data) ? data.length > 0 : Boolean(data));
+                const res = await apiCall.get("/userSubscription/user-subscription-active");
+                const data: UserSubscriptionActiveDTO = res.data?.data;
+                if (!data) {
+                    if (mounted) setActive(false);
+                } else {
+                    if (mounted) setActive(data.payment_status === "APPROVED" ? true : false);
+                }
             } catch {
                 if (mounted) setActive(false);
             }
